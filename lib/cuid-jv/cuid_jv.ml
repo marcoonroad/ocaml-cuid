a08a5f4e44ee070436190a310f6b84042d1f82dc
Martin Schut <martin-gitlab@wommm.nl>
2022-02-18 22:32:19 +0100

    Fix bug in jv

--- lib/cuid-jv/cuid_jv.ml
+++ lib/cuid-jv/cuid_jv.ml
@@ -2,7 +2,7 @@ include Cuid.Make (struct
   open Brr
 
   let mime_types_length = match Jv.find (Jv.Id.to_jv G.navigator) "mimeTypes" with
-    | Some n -> Jv.to_int n
+    | Some a -> Jv.Jarray.length a
     | None -> 0
 
   let user_agent_length = match Jv.find (Jv.Id.to_jv G.navigator) "userAgent" with
@@ -11,8 +11,8 @@ include Cuid.Make (struct
 
   let variables_in_global_scope =
     let o = Jv.get Jv.global "Object" in
-    let k = Jv.call o "keys" Jv.[| (Id.to_jv G.window) |] in
-    Jv.Jarray.length k
+    let a = Jv.call o "keys" Jv.[| (Id.to_jv G.window) |] in
+    Jv.Jarray.length a
 
   let value =
     let id1 =
